a7770deec8f25860f7000087dd73e1b4c15345e2
rkierbel <rudykierbel@gmail.com>
2023-05-23 15:40:07 +0200

    moved holiday enum into country class

--- src/main/java/com/bankholiday/country/Belgium.java
+++ src/main/java/com/bankholiday/country/Belgium.java
@@ -2,7 +2,8 @@ package com.bankholiday.country;
 
 import com.bankholiday.holiday.typology.CelebratesEaster;
 import com.bankholiday.holiday.IsKnownHoliday;
-import com.bankholiday.holiday.percountry.BelgianHoliday;
+
+import java.time.LocalDate;
 
 public class Belgium implements IsKnownCountry, CelebratesEaster {
 
@@ -22,6 +23,31 @@ public class Belgium implements IsKnownCountry, CelebratesEaster {
 
   @Override
   public IsKnownHoliday[] holidays() {
-    return BelgianHoliday.values();
+    return Holiday.values();
+  }
+
+  public enum Holiday implements IsKnownHoliday, CelebratesEaster {
+    NEW_YEAR(1, 1),
+    LABOUR_DAY(5, 1),
+    NATIONAL_DAY(7, 21),
+    ASSUMPTION(8, 15),
+    ALL_SAINTS(11, 1),
+    ALL_SOULS(11, 2),
+    ARMISTICE(11, 11),
+    KINGS_FEAST(11, 15),
+    CHRISTMAS(12, 25),
+    DAY_AFTER_CHRISTMAS(12, 26);
+
+    private final int month;
+    private final int day;
+
+    Holiday(int month, int day) {
+      this.month = month;
+      this.day = day;
+    }
+
+    public LocalDate date(int year) {
+      return LocalDate.of(year, this.month, this.day);
+    }
   }
 }
